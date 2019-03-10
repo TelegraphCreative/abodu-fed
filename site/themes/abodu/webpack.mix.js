17b349943b0cf60b2eda284ce5a9827537455b44
Tyler <tylernford@gmail.com>
2019-03-10 15:29:24 -0500

    :heavy_plus_sign: :wrench: Adds fresh theme + Mix
    Adds new theme (_Abodu_) + Mix with SASS, Tailwind, PostCSS, Purgecss

---
+++ site/themes/abodu/webpack.mix.js
@@ -0,0 +1,34 @@
+let mix = require('laravel-mix');
+
+let tailwindcss = require('tailwindcss');
+
+require('laravel-mix-purgecss');
+
+mix.sass('sass/styles.scss', 'css/abodu.css')
+	.options({
+		processCssUrls: false,
+		postCss: [tailwindcss('tailwind.js')],
+	});
+   /*
+   .js([
+      'js/base.js',
+   ], 'js/my-theme.js');
+   */
+
+mix.disableSuccessNotifications();
+
+if (mix.inProduction()) {
+    mix.purgeCss({
+        enabled: true,
+        globs: [
+            path.join(__dirname, 'layouts/*.html'),
+            path.join(__dirname, 'templates/*.html'),
+            path.join(__dirname, 'templates/**/*.html'),
+            path.join(__dirname, 'partials/*.html'),
+            path.join(__dirname, 'partials/**/*.html'),
+            path.join(__dirname, 'js/**.js'),
+            path.join(__dirname, 'img/**.svg'),
+        ],
+        extensions: ['html', 'js', 'php', 'svg'],
+    })
+};
